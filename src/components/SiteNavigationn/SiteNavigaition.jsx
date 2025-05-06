35544e7f5e07e9c65b8f761d0ba3c641c3a241ab
Bobby Marko <bobbymarkodesign@gmail.com>
2025-05-06 13:33:54 -0500

    fix: move home page back to root path /

--- src/components/SiteNavigationn/SiteNavigaition.jsx
+++ src/components/SiteNavigationn/SiteNavigaition.jsx
@@ -35,7 +35,7 @@ function SiteNavigaition() {
       boxShadow: '2px 0 5px rgba(0, 0, 0, 0.2)',
     }}>
       <p style={{ color: 'black', marginLeft: '10px', marginBottom: '5px' }}>Public</p>
-      <Link to="/home" style={buttonStyle}>Home</Link>
+      <Link to="/" style={buttonStyle}>Home</Link>
       <Link to="/orders" style={buttonStyle}>Order</Link>
       <Link to="/Thanks" style={buttonStyle}>Thanks</Link>
 
